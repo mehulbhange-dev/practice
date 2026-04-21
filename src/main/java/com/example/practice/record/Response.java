d1d0e8ac2cd9dbf66613970d16c6d164dfa9af8a
Mehul-Test <mehulbhange.dev@gmail.com>
2026-04-21 12:27:27 +0530

    Revert "CI/CD pipeline practice"
    This reverts commit e8e5bd5295a752cef953bb294ec670008b421c3f.

--- src/main/java/com/example/practice/record/Response.java
+++
@@ -1,6 +0,0 @@
-package com.example.practice.record;
-
-import java.time.LocalDateTime;
-
-public record Response<T>(T data, boolean success, int status, String message, LocalDateTime timestamp) {
-}
